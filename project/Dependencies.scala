a0c9951232bb990c904791216b556de7f8539b91
Matemaatikko <juho_salmensuu@hotmail.com>
2015-09-11 17:12:05 +0300

    fix.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,12 +7,12 @@ object Dependencies {
 
   // Libraries
   object Version {
-    val scalaTest = "2.0"
+    val scalaTest = "2.2.4"
     val mockito = "1.9.5"
     val scalaLogging = "2.1.2"
   }
 
-  val scalaTest = "org.scalatest" % "scalatest_2.10" % Version.scalaTest
+  val scalaTest = "org.scalatest" % "scalatest_2.11" % Version.scalaTest
   val mockito = "org.mockito" % "mockito-all" % Version.mockito
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging-slf4j" % Version.scalaLogging
 
